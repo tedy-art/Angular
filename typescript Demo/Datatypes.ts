318548cbf66fb3dc2100bcb2e6a6d6d1f5cf454c
Tejas Falke <75989281+tedy-art@users.noreply.github.com>
2024-03-22 19:54:40 +0530

    new update

---
+++ typescript Demo/Datatypes.ts
@@ -0,0 +1,29 @@
+// we can declare variable before declaring it's value
+let fname : string;
+
+// Declaring sting values
+fname = 'Tejas';
+
+// method we can use on string
+let newname = fname.toUpperCase();
+console.log(newname)
+
+
+console.log('lower : ',newname.toLowerCase());
+console.log('trim : ',newname.trim());
+console.log('length : ',newname.length);
+
+
+//Numbers
+// declaration of data type
+let age : number;
+
+age = 25;
+age = 25.5;
+console.log('age : ', age);
+
+// Convert string to number
+let en_marks = "68";
+let Convert_to_int = parseInt(en_marks);
+console.log('mark of English : ',Convert_to_int);
+
